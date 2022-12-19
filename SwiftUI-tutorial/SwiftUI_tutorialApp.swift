b4f52a3f9805e9172f8435c29daf51671d8e0566
정윤서 <jeong-yuunseo@jeong-yunseoui-MacBookPro.local>
2022-12-19 23:24:14 +0900

    first commit

---
+++ SwiftUI-tutorial/SwiftUI_tutorialApp.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+@main
+struct SwiftUI_tutorialApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
